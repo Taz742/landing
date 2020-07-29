feb063c627726897a9a3ecd50474f10d54d113a0
daraselia <zdaras@brokers.io>
2020-07-29 16:04:16 +0400

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Head from 'next/head';
 
 import { getInitialLocale } from '@/translations/get-initial-locale';
+import config from '@/utils/config';
 
 const Index: React.FC = () => {
+  const url = `${config.url}/en`;
+
   React.useEffect(() => {
     window.location.replace(`/${getInitialLocale()}`);
   }, []);
 
   return (
     <Head>
-      <meta name="robots" content="noindex, nofollow" />
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href={url} />
     </Head>
   );
 };
