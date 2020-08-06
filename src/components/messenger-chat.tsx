fbbd6c13f3319256929a508c304e74bd5158d45d
daraselia <zdaras@brokers.io>
2020-08-06 18:35:34 +0400

    .

--- src/components/messenger-chat.tsx
+++ src/components/messenger-chat.tsx
@@ -9,7 +9,7 @@ const MessengerChat: React.FC = () => {
   const { locale } = useTranslation();
 
   const language = locale === 'ka' ? 'ka_GE' : 'en_US';
-  const loggedInGreeting = locale === 'ka' ? 'რით დაგეხმაროთ?' : 'How can we help?';
+  const loggedInGreeting = locale === 'ka' ? 'მოგესალმებით! რით შეგვიძლია დაგეხმაროთ?' : 'Welcome! How can we help?';
 
   return <MessengerCustomerChat pageId={config.fbPageId} appId={config.fbAppId} language={language} loggedInGreeting={loggedInGreeting} />;
 };
